317233683bde35b2f78445432ce0f18f9b5b4e60
wuyuan <wrzmonkey2013@hotmail.com>
2019-02-18 16:27:18 +0800

    Updates

---
+++ precache-manifest.dd7c4e24dd2eb05b539a06d505e5e70c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "56e192ed3e2d64896008",
+    "url": "/static/js/main.56e192ed.chunk.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "56e192ed3e2d64896008",
+    "url": "/static/css/main.081dd6f0.chunk.css"
+  },
+  {
+    "revision": "0bfce9567f9042b56734e8eea30fb242",
+    "url": "/index.html"
+  }
+];

0c46511c58165a95a1db29e5cdd1ece05e02e300
wuyuan <wrzmonkey2013@hotmail.com>
2019-02-18 10:02:21 +0800

    Updates

---
+++ precache-manifest.46784e4328ace4b2bb36c2a1123b9c02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitpage_demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3cc4f0085913b3f2917a",
+    "url": "/gitpage_demo/static/js/runtime~main.3cc4f008.js"
+  },
+  {
+    "revision": "f547973df13199449000",
+    "url": "/gitpage_demo/static/js/main.f547973d.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/gitpage_demo/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "f547973df13199449000",
+    "url": "/gitpage_demo/static/css/main.081dd6f0.chunk.css"
+  },
+  {
+    "revision": "98a63bdf545d7aa61ef30a41cc7125ed",
+    "url": "/gitpage_demo/index.html"
+  }
+];

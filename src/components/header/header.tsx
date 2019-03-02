d1029ac4ab3d889b279965901023af2fd8a66b67
ElonWu <wrzmonkey2013@hotmail.com>
2019-03-03 06:16:51 +0800

    样式修改

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -22,25 +22,27 @@ export class Header extends React.Component<Props, State> {
         className={`header ${cover && "cover"}`}
         onClick={() => this.setState({ cover: !cover })}
       >
-        <div className="avatar">
-          <img src={avatar} alt="" width="100%" />
-        </div>
-        <div className="info">
-          <h3> 吴渊 (Elon Wu) </h3>
-          <ul className="info-detail">
-            <li>
-              <Icon type="calendar" />
-              <span>27岁</span>
-            </li>
-            <li>
-              <Icon type="code" />
-              <span>Web 前端</span>
-            </li>
-            <li>
-              <Icon type="dashboard" />
-              <span>3年</span>
-            </li>
-          </ul>
+        <div className="header-container">
+          <div className="avatar">
+            <img src={avatar} alt="" width="100%" />
+          </div>
+          <div className="info">
+            <h3> 吴渊 (Elon Wu) </h3>
+            <ul className="info-detail">
+              <li>
+                <Icon type="calendar" />
+                <span>27岁</span>
+              </li>
+              <li>
+                <Icon type="code" />
+                <span>Web 前端</span>
+              </li>
+              <li>
+                <Icon type="dashboard" />
+                <span>3年</span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     );
